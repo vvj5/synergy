3499d1e9250df778fade1efbfb6ddb4ad5af62ae
Will Sherman <vvj5@icloud.com>
2015-06-25 09:43:27 -0500

    Finally got rake db migrate to work

--- db/migrate/20150625002627_create_courses.rb
+++ db/migrate/20150625002627_create_courses.rb
@@ -2,7 +2,7 @@ class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
       t.string :title
-      t.belongs_to :locations, index: true, foreign_key: true
+      t.belongs_to :location, index: true, foreign_key: true
 
       t.timestamps null: false
     end
